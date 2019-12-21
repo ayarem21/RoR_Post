7e435ba465860650d10907a78ba8f2b94a4f9aea
ayarem21 <ayarem21@gmail.com>
2019-12-21 15:18:03 +0200

    fix ajax add && edit && reply comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,11 +14,6 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
     @comment.author_id = current_user.id
-    #redirect_to post_path(@post)
-    #if @comment.save!
-    # flash.now[:danger] = "error"
-    # format.js
-    #end
     if @comment.ancestors.count <= 4
       respond_to do |format|
         if @comment.save
@@ -35,34 +30,37 @@ class CommentsController < ApplicationController
   end
 
   def edit
-    @comment = Comment.find(params[:id])
-    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
   end
 
- def update
-   @post = Post.find(params[:post_id])
-   @comment = Comment.find(params[:id])
-   if @coment.ancestors.count <=5
-     respond_to do |format|
-       if @comment.update(comment_params)
-         format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-         format.json { render :show, status: :ok, location: @post }
-         format.js
-       else
-         format.html { render :edit }
-         format.json { render json: @post.errors, status: :unprocessable_entity }
-         format.js
-       end
+  def update
+    @comment = @post.comments.find(params[:id])
+    if @comment.ancestors.count <= 5
+      respond_to do |format|
+        if @comment.update(comment_params)
+          format.js
+        else
+          format.html { render :edit }
+        end
+      end
+    end
+  end
 
-     end
-   end
+  def destroy
+    respond_to do |format|
+      if @comment.destroy
+        format.js { render 'destroy', status: :created, location: @post }
+      else
+        format.html { redirect_to @post, alert: 'Error!' }
+      end
+    end
   end
 
 
   private
 
   def comment_params
-    params.require(:comment).permit(:body, :author_id, :parent_id)
+    params.require(:comment).permit(:body, :author_id, :post_id, :parent_id)
   end
 
 
